81ffb4e452b02c33e339a26bce5a91e49585e461
fredkeem <fred@zikto.com>
2018-10-22 13:25:39 +0900

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,57 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-app id="inspire">
+    <v-toolbar
+      style="max-width: 100%; display: block"
+      color="#1967AE"
+      dark
+      justify-center
+      align-center
+    >
+      <v-toolbar-title
+        justify-center
+        align-center
+        style="display: block; margin: 0 auto"
+        class="title"
+      >
+        정보 등록
+      </v-toolbar-title>
+    </v-toolbar>
+
+    <v-content style="padding: 0;">
+      <section style="background-color: #0967AE;">
+        <v-parallax height="60" >
+          <v-layout row align-center justify-center d-flex>
+            <div class="white--text mb-3 text-xs-center">
+              <p style="width: 40px; height: 40px; border: 1px solid; border-radius: 50%;">1</p>
+              <em>상품확인</em>
+            </div>
+            <div class="white--text mb-3 text-xs-center">
+              <p style="width: 40px; height: 40px; border: 1px solid; border-radius: 50%;">1</p>
+              <em>상품확인</em>
+            </div>
+            <div class="white--text mb-3 text-xs-center">
+              <p style="width: 40px; height: 40px; border: 1px solid; border-radius: 50%;">1</p>
+              <em>상품확인</em>
+            </div>
+            <div class="white--text mb-3 text-xs-center">
+              <p style="width: 40px; height: 40px; border: 1px solid; border-radius: 50%;">1</p>
+              <em>상품확인</em>
+            </div>
+          </v-layout>
+        </v-parallax>
+      </section>
+    </v-content>
+    <v-footer>
+      <v-btn
+        style="width: 100%; display: block; margin: 0;"
+        color="#1967AE"
+        dark
+        class="title"
+      >
+        등록
+      </v-btn>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
